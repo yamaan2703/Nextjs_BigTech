9951f953f9e85f2b56b13f94724dae7c19b9800d
Muhammad Yamaan <yamaan004@gmail.com>
2024-07-17 21:50:04 +0500

    fix: Navbar UI & Complete Navbar

---
+++ big-tech/src/components/atoms/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button(props:any) {
+    const {label, onClick} = props
+  return (
+    <button
+  className="relative px-8 py-3 rounded-full isolation-auto z-10 border-2 border-[#00c4f4] before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#00c4f4] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold"
+>
+  {label}
+</button>
+
+  )
+}
+
+export default Button
--- big-tech/src/components/atoms/Navbar/Navbar.tsx
+++ big-tech/src/components/atoms/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { IoMenuSharp } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button/Button";
 
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -11,15 +12,41 @@ function Navbar() {
     <div className="text-white relative">
       <div className="flex items-center px-5 justify-between border-b border-gray-800">
         <div className="w-24 h-24 py-4">
-          <Image src={"/images/logo.png"} alt="Logo" width={1000} height={1000} />
+          <Image
+            src={"/images/logo.png"}
+            alt="Logo"
+            width={1000}
+            height={1000}
+          />
         </div>
-        <div className="hidden md:flex gap-5 font-semibold">
-          <p>HOME</p>
-          <p>ABOUT US</p>
-          <p>SALES</p>
-          <p>ROADMAP</p>
-          <p>BLOGS</p>
-          <p>CONTACT US</p>
+        <div className="hidden md:flex gap-10 font-semibold">
+          <p className="relative group pb-2">
+            HOME
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="relative group pb-2">
+            ABOUT US
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="relative group pb-2">
+            SALES
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="relative group pb-2">
+            ROADMAP
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="relative group pb-2">
+            BLOGS
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="relative group pb-2">
+            CONTACT US
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+        </div>
+        <div className="hidden md:block">
+          <Button label="BUY NOW" />
         </div>
         <div className="md:hidden">
           <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
@@ -36,16 +63,40 @@ function Navbar() {
           isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 ease-in-out`}
       >
-        <button onClick={() => setIsMobileMenuOpen(false)} className="absolute top-4 right-4">
+        <button
+          onClick={() => setIsMobileMenuOpen(false)}
+          className="absolute top-4 right-4"
+        >
           <IoMdClose className="h-6 w-6 text-white" />
         </button>
         <div className="flex flex-col items-start py-2 px-5 mt-10">
-          <p className="py-2 hover:text-[#00c4f4] w-full">HOME</p>
-          <p className="py-2 w-full">ABOUT US</p>
-          <p className="py-2 w-full">SALES</p>
-          <p className="py-2 w-full">ROADMAP</p>
-          <p className="py-2 w-full">BLOGS</p>
-          <p className="py-2 w-full">CONTACT US</p>
+          <p className="py-2 w-full relative group">
+            HOME
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="py-2 w-full relative group">
+            ABOUT US
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="py-2 w-full relative group">
+            SALES
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="py-2 w-full relative group">
+            ROADMAP
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="py-2 w-full relative group">
+            BLOGS
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+          <p className="py-2 w-full relative group">
+            CONTACT US
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#00c4f4] transition-all duration-300 group-hover:w-full"></span>
+          </p>
+        </div>
+        <div className="absolute bottom-0 w-full p-4 bg-#030b15">
+          <Button label="BUY NOW" />
         </div>
       </div>
     </div>
